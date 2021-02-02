5cd2137ec5259726b0e7d9de9831cde06e7a258c
Dhuha9 <dhuha9hasan@gmail.com>
2021-02-03 01:10:22 +0300

    Project structuring Fixes #1

---
+++ hangman/src/Components/LetterBtns.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class LetterBtns extends Component {
+	render() {
+		return <div></div>;
+	}
+}
---
+++ hangman/src/Components/MatchedLetters.jsx
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class MatchedLetters extends Component {
+	render() {
+		return <div></div>;
+	}
+}
