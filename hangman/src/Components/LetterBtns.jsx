30254c48559d0c4b5c55a0de4bd9cd3d608586bb
unknown <ismail.th.khalil@gmail.com>
2021-02-03 02:26:30 +0300

    Handle letter buttons  Fixes #10

--- hangman/src/Components/LetterBtns.jsx
+++ hangman/src/Components/LetterBtns.jsx
@@ -2,11 +2,25 @@ import React, { Component } from 'react'
 
 export default class Letters_Btn extends Component {
 
-	
+	constructor() {
+		super()
+		this.state = {
+			pressed: new Set()
+		}
+
+		this.clickedButton = this.clickedButton.bind(this)
+	}
+
+	clickedButton = (event) => {
+		this.setState({
+			pressed: this.state.pressed.add(event.target.name)
+		})
+	}
+
     render() {
         return (
 			<div>
-				{this.props.letters.map( item => <button name={item} value={item}>{item}</button>)}
+				{this.props.letters.map( item => <button name={item} value={item} disabled={this.state.pressed.has(item)} onClick={this.clickedButton} >{item}</button>)}
 			</div>
 		)
     }
