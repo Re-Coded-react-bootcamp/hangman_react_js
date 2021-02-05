4dbed9117356eaeb4fff35cb9d1f582c10203232
Dhuha9 <dhuha9hasan@gmail.com>
2021-02-05 16:01:25 +0300

    guessedLetter error of undefind fixed

--- hangman/src/Components/Header.jsx
+++ hangman/src/Components/Header.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import logo from "../Images/logo.png";
-import '../Styles/Header.css'
-
+import logo from '../Images/logo.png';
+import '../Styles/Header.css';
 
 export default class Header extends Component {
-	render() {
-		return (
-			<div>
-			        <img src={logo} className="App-logo" alt="logo" />
-				<h3 className="lead">Use the alphabet below to guess the word, or click hint to get a clue.</h3>
-			</div>
-		)
-	}
+  render() {
+    return (
+      <div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3 className="lead">
+          Use the alphabet below to guess the word, or click hint to get a clue.
+        </h3>
+      </div>
+    );
+  }
 }
