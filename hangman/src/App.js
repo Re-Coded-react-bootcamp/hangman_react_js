35fc8f682a4423d54e43b5c1d806c80abe0f4283
ZETTA <faeza.smohammed@gmail.com>
2021-02-05 15:40:41 +0300

    Footer design and styling Fixes #19

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Main from './Components/Main';
 import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
 			<Main />
+			<Footer />
 		</div>
 	);
 }
---
+++ hangman/src/Components/Footer.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import '../Styles/Footer.css'
+
+export default class Footer extends Component {
+    render() {
+        return (
+        <footer className="text-center text-lg-start">
+            <div className="container mt-auto"> 
+           <div className="row justify-content-center">
+           <div className="col-2">
+            <button className="btn btn-outline-dark">Play again
+            </button>
+        </div> 
+            <div className="col-2">
+            <button className="btn btn-outline-dark">Hint
+            </button> 
+        </div> 
+        </div> 
+            </div>
+        </footer>
+    )
+    }
+}
