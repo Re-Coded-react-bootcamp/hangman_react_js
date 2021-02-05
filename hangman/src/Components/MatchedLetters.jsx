7240b833c18a1d6f494f0facc5810cb9a1258c3d
Dhuha9 <dhuha9hasan@gmail.com>
2021-02-05 10:40:05 +0300

    Displaying guessed letters Fixes #16

--- hangman/src/Components/MatchedLetters.jsx
+++ hangman/src/Components/MatchedLetters.jsx
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-
+import '../Styles/MatchedLetters.css';
 export default class MatchedLetters extends Component {
   render() {
     let { guessedLetters, word } = this.props;
 
     return (
       <div>
-        <p>_</p>
-        <p>{'  '}</p>
+        {word.split('').map((letter) => {
+          let isLetterMached = guessedLetters.has(letter);
+          return (
+            <>
+              <p
+                className={`letters ${
+                  isLetterMached ? 'black-text' : 'white-text'
+                }`}
+              >
+                {isLetterMached ? letter : '*'}
+              </p>
+              <p className="dashes">{'  '}</p>
+            </>
+          );
+        })}
       </div>
     );
   }
